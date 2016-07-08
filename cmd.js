aff1124382445d25449c59f9864e0e25c07d9669
Kazuho Oku <kazuhooku@gmail.com>
2016-07-08 16:18:39 +0900

    add command-line client

---
+++ cmd.js
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2016 DeNA Co., Ltd., Kazuho Oku
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+"use strict";
+
+var vm = require("vm");
+var fs = require("fs");
+
+vm.runInThisContext(fs.readFileSync("cache-digest.js", "ascii"));
+
+process.exit(function (argv) {
+    var pbits = 7;
+    var useBase64 = false;
+
+    argv.shift();
+    argv.shift();
+    while (argv.length != 0 && argv[0].match(/^-/) != null) {
+        var opt = argv.shift();
+        if (opt == "-")
+            break;
+        if (opt == "-h" || opt == "--help") {
+            console.log("Usage: node cmd.js [-b] [-p=pbits] URL1 URL2...")
+            return 0;
+        } else if (opt == "-b") {
+            useBase64 = 1;
+        } else if (opt.match(/^-p(?:=(.*)|$)/) != null) {
+            if (RegExp.$1 != null) {
+                pbits = RegExp.$1;
+            } else if (argv.length == 0) {
+                console.error("argument value missing for option: -p");
+                return 1;
+            } else {
+                pbits = argv.shift();
+            }
+        } else {
+            console.error("Unknown option: %s", opt);
+            return 1;
+        }
+    }
+
+    var digest = calcDigestValue(argv, pbits);
+    if (pbits == null) {
+        console.error("failed to calculate the digests");
+        return 1;
+    }
+
+    if (useBase64) {
+        console.log("%s", base64Encode(digest));
+    } else {
+        fs.createWriteStream(null, {fd: 1, defaultEncoding: "binary"}).write(Buffer.from(digest));
+    }
+
+    return 0;
+}(process.argv));
